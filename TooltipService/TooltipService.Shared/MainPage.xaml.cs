09873c28092cd28bfc6960c587b12be3cee48401
Paulo Vilá <paulovila@live.com>
2022-08-20 01:37:24 +0100

    Add project files.

---
+++ TooltipService/TooltipService.Shared/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+namespace TooltipService
+{
+    public class P1 {
+    public string S1 { get; set; }
+    }
+    public class VM {
+        public P1  P1 {get;set;}
+    }
+    
+    public sealed partial class MainPage 
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataContext = new VM { P1 = new P1 { S1 = "Yes!" } };
+        }
+    }
+}
